086acf173c80dc5b40022e8368230613c34ccf48
EndGhost214 <112347364+EndGhost214@users.noreply.github.com>
2022-10-22 19:20:36 -0700

    Working sql query

--- dashboard.php
+++ dashboard.php
@@ -4,67 +4,73 @@
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>Dashboard - Client area</title>
     <link rel="stylesheet" href="style.css" />
 </head>
+
 <body>
     <div class="form">
         <p>Hey, user!</p>
         <p>You are now logged into the user dashboard page.</p>
         <hr>
     </div>
-    
-<div>
+
+    <div>
+
+        <form method="post">
+            <p>SQL Query: <input type="text" name="SQL" /></p>
+            <p><input type="submit" /></p>
+        </form>
 
 
+        <table>
+            <tr>
+                <th>TiD</th>
+                <th>Name</th>
+                <th>Location</th>
+                <th>Cuisine</th>
+                <th>Rating</th>
+                <th>Price</th>
+                <th>Dining</th>
 
-
-    <table>
-        <tr>
-            <th>TiD</th>
-            <th>Name</th>
-            <th>Location</th>
-            <th>Cuisine</th>
-            <th>Rating</th>
-            <th>Price</th>
-            <th>Dining</th>
-
-</tr>
-    <?php
+            </tr>
+            <?php
     require('db.php');
-    $sql = "select * from restaurants";
+    $sql = $_POST['SQL'];
     $result = $con->query($sql);
     $sr =1;
 
    
     while($row = $result->fetch_assoc()){?>
-    <tr>
-        <form action="" method="post" role="form">
-            <td><?php echo $sr;?></td>
-            <td><?php echo $row['Name'];?></td>
-            <td><?php echo $row['Location'];?></td>
-            <td><?php echo $row['Cuisine'];?></td>
-            <td><?php echo $row['Rating'];?></td>
-            <td><?php echo $row['Price'];?></td>
-            <td><?php echo $row['Dining'];?></td>
-            
-        </form>
-    </tr>
-        <?php $sr++; }
+            <tr>
+                <form action="" method="post" role="form">
+                    <td><?php echo $sr;?></td>
+                    <td><?php echo $row['Name'];?></td>
+                    <td><?php echo $row['Location'];?></td>
+                    <td><?php echo $row['Cuisine'];?></td>
+                    <td><?php echo $row['Rating'];?></td>
+                    <td><?php echo $row['Price'];?></td>
+                    <td><?php echo $row['Dining'];?></td>
+
+                </form>
+            </tr>
+            <?php $sr++; }
         ?>
-</table>
+        </table>
 
 
-</div>
+    </div>
 
 
 
 
 
-    
+
 </body>
+
 </html>
 
 <!--
