3b75afb26034caeb4286c03469b247c38742a1c9
mj3272 <marissasamayoa2@gmail.com>
2022-11-01 19:11:14 -0700

    oops

--- table.sql
+++ table.sql
@@ -1,11 +1,4 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  Marissa
- * Created: Nov 1, 2022
- */
+
 
 
 CREATE TABLE restaurants(
