875634775ef11913774b9b1571ca7c7fb4dad3dd
EndGhost214 <112347364+EndGhost214@users.noreply.github.com>
2022-11-20 21:54:44 -0800

    added onto parser function

--- parse.php
+++ parse.php
@@ -2,6 +2,28 @@
 function parse($x) {
     $array = explode(" ", $x);
     print_r($array);
+    echo "<br>";
+
+    foreach($array as $value){
+        echo $value . ' ';
+
+        if(str_contains($value, "Location")){
+            echo "Yippie!";
+        }
+        if(str_contains($value, "Cuisine")){
+            echo "Yippie!";
+        }
+        if(str_contains($value, "Rating")){
+            echo "Yippie!";
+        }
+        if(str_contains($value, "Price")){
+            echo "Yippie!";
+        }
+        if(str_contains($value, "Dining")){
+            echo "Yippie!";
+        }
+
+    }
 }
         
     
