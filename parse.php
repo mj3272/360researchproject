5ba94c45f5a299a61986169cd885935736ec8c90
EndGhost214 <112347364+EndGhost214@users.noreply.github.com>
2022-11-27 15:04:17 -0800

    Updated parse function

--- parse.php
+++ parse.php
@@ -4,22 +4,34 @@ function parse($x) {
     print_r($array);
     echo "<br>";
 
+    $location = '';
+    $cuisine = '';
+    $rating = '';
+    $price = '';
+    $dining = '';
+
     foreach($array as $value){
         echo $value . ' ';
 
         if(str_contains($value, "Location")){
+            $location = $location . $value;
+            
             echo "Yippie!";
         }
         if(str_contains($value, "Cuisine")){
+            $cuisine = $cuisine . $value;
             echo "Yippie!";
         }
         if(str_contains($value, "Rating")){
+            $rating = $rating . $value;
             echo "Yippie!";
         }
         if(str_contains($value, "Price")){
+            $price = $price . $value;
             echo "Yippie!";
         }
         if(str_contains($value, "Dining")){
+            $dining = $dining . $value;
             echo "Yippie!";
         }
 
